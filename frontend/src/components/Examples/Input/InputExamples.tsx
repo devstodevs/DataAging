4316bbe9370c00b199dc916dd744607209ca9f1d
AllanSilva29 <allanjulio61@gmail.com>
2025-08-19 00:33:39 -0300

    Placeholder para exemplos futuros

---
+++ frontend/src/components/Examples/Input/InputExamples.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ExampleContainer from "../ExampleContainer";
+
+const InputExamples: React.FC = () => {
+  return (
+    <ExampleContainer
+      title="Input Component Examples"
+      description="Different input types and validation states"
+      size="medium"
+    >
+      <p style={{ color: "#6B7280" }}>
+        Input component examples will be added here when the Input component is
+        created.
+      </p>
+    </ExampleContainer>
+  );
+};
+
+export default InputExamples;

fb32d49058e213e1579e416d328431592b8b907a
AllanSilva29 <allanjulio61@gmail.com>
2025-10-05 04:12:03 -0300

    [mod] Ajuste de estrutura das pastas

--- backend/src/backend/__init__.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from backend.auth import router as auth_router
-
-app = FastAPI()
-app.include_router(auth_router)
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.auth import router as auth_router
+from api.auth import router as auth_router
 
 app = FastAPI()
 
